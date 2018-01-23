2dfb28096998b54bd94d587025aab1b42798507b
iwasanuradhi <iwasanuradhi@virtusa.com>
2018-01-23 19:17:11 +0530

    Dev_2

--- new 2.java
+++ new 2.java
@@ -1,7 +1,9 @@
 public class FortressTest {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println("Hello Fortress Team!!!...");
+		System.out.println("Developer_1...");
+		System.out.println("Developer_2...");
+		
     }
 
 }
